03b5a5e7d7e9657e6b5f2ecc8389d6b8b6c613c2
isthar <isthar@IMPALA>
2015-08-19 21:05:10 +0200

    log del sistema

---
+++ src/main/java/es/bestbikes/util/Trazas.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.bestbikes.util;
+
+/**
+ *
+ * @author isthar
+ * TODO conectar con algo tipo log4j
+ */
+public class Trazas {
+    
+     
+    public static void trazar(String mensaje) {
+        System.out.println(mensaje);
+    }
+    
+}
